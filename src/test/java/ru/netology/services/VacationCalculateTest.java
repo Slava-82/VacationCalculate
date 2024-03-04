546c5c15675eee5122a1dd49f0c432d90cee2bda
Slava-82 <utsznslava@gmail.com>
2024-03-04 12:06:12 +0300

    ParameterizedTest

--- src/test/java/ru/netology/services/VacationCalculateTest.java
+++ src/test/java/ru/netology/services/VacationCalculateTest.java
@@ -1,91 +1,88 @@
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
-//import ru.netology.services.VacationCalculate;
-//
-//public class VacationCalculateTest {
-//
-//    @ParameterizedTest
-//    @CsvSource({
-//            "14888, 10000, 3000, 20000",
-//            "1000, 100000, 60000, 150000"
-//    })
-//    public void firstTest(int expected, int income, int expenses, int threshold) {
-//        VacationCalculate service = new VacationCalculate();
-//
-//        // int expected = 14888;
-//        int actual = service.calculate(income, expenses, threshold);
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
-//}
+
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class VacationCalculateTest {
-    @Test
-    public void MonthVacationExample() {
+    @ParameterizedTest
+//    @CsvSource ({
+//    "3,10000,3000,20000",
+//    "2,100000,60000,150000",
+//            "12,0,0,0",
+//            "11,90000,0,1",
+//            "1,1100,100,11000",
+//            "0,10,3,77001"
+//})
+    @CsvFileSource(resources = "/Vacation.csv")
+    public void MonthVacationParameterizedTest(int expected, int income, int expenses, int treshold) {
         VacationCalculate service = new VacationCalculate();
-
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
+        int actual = service.calculate(income, expenses, treshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void MonthVacationSecondExample() {
-        VacationCalculate service = new VacationCalculate();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void maxMonthVacation() {
-        VacationCalculate service = new VacationCalculate();
-
-        int expected = 12;
-        int actual = service.calculate(0, 0, 0);
-
-        Assertions.assertEquals(expected, actual);
-
-
-    }
-
-    @Test
-    public void elevenMonthVacation() {
-        VacationCalculate service = new VacationCalculate();
-
-        int expected = 11;
-        int actual = service.calculate(90000, 0, 1);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void minMonthVacation() {
-        VacationCalculate service = new VacationCalculate();
-
-        int expected = 1;
-        int actual = service.calculate(1100, 100, 11000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nullMonthVacation() {
-        VacationCalculate service = new VacationCalculate();
-
-        int expected = 0;
-        int actual = service.calculate(10000, 3000, 77001);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
+//    @Test
+//    public void MonthVacationExample() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 3;
+//        int actual = service.calculate(10000, 3000, 20000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void MonthVacationSecondExample() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void maxMonthVacation() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 12;
+//        int actual = service.calculate(0, 0, 0);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//
+//    }
+//
+//    @Test
+//    public void elevenMonthVacation() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 11;
+//        int actual = service.calculate(90000, 0, 1);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void minMonthVacation() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 1;
+//        int actual = service.calculate(1100, 100, 11000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void nullMonthVacation() {
+//        VacationCalculate service = new VacationCalculate();
+//
+//        int expected = 0;
+//        int actual = service.calculate(10000, 3000, 77001);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
 }
